bc3561325cf751b2c7f2e2ce0dd9e0a36920f9f7
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2024-04-12 12:15:41 +0530

    Completed Hero Section

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -1,30 +1,36 @@
 import Link from 'next/link'
 import React from 'react'
-// import { Button } from "@/components/ui/moving-border";
+import { Spotlight } from './ui/Spotlight'
+import { Button } from "@/components/ui/moving-border";
 
 function Herosection() {
   return (
     <div
-      className='h-auto md:h[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'
+      className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'
     >
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="gray"
+      />
       <div className='p-4 relative z-10 w-full text-center'>
+      
          <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
             >Master the art of music</h1>
 
 
-         <p className="4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate, aperiam exercitationem 
             repudiandae sint a facere adipisci. Praesentium inventore fugit aliquid pariatur 
             at qui aliquam sequi et architecto eos, ratione voluptates alias nemo dolorum dolores.</p>
 
             <div className="mt-4">
                 <Link href={"/courses"}>
-                    <button
-                  //   borderRadius="1.75rem"
+                    <Button
+                    borderRadius="1.75rem"
                     className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
                     >
                     Explore courses
-                    </button>
+                    </Button>
                 </Link>
             </div>
       </div>
